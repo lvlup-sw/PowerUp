2f926644ca422bcb4d1853203fe9499381fa766a
Reed Salus <reedsalus@gmail.com>
2024-09-08 00:12:48 -0700

    tests

--- src/PowerUp.Tests/CacheKeyGeneratorTests.cs
+++ src/PowerUp.Tests/CacheKeyGeneratorTests.cs
@@ -1,12 +1,110 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using PowerUp.Caching;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace PowerUp.Tests
 {
-    internal class CacheKeyGeneratorTests
+    [TestClass]
+    public class CacheKeyGeneratorTest
     {
+        [TestMethod]
+        public void EmptyInput_ReturnsCorrectHash()
+        {
+            // Arrange
+            ReadOnlySpan<byte> bytes = default;
+            uint seed = 12345;
+
+            // Act
+            uint result = CacheKeyGenerator.Hash32(ref bytes, seed);
+
+            // Assert
+            Assert.AreEqual(seed, result);
+        }
+
+        [TestMethod]
+        public void DifferentSeeds_HashesAreDistinct()
+        {
+            // Arrange
+            var random = new Random();
+            var bytes = new byte[64];
+            random.NextBytes(bytes);
+            var bytesSpan1 = new ReadOnlySpan<byte>(bytes);
+            random.NextBytes(bytes);
+            var bytesSpan2 = new ReadOnlySpan<byte>(bytes);
+
+            // Act
+            uint seed1 = 42;
+            uint hash1 = CacheKeyGenerator.Hash32(ref bytesSpan1, seed1);
+
+            uint seed2 = 12345;
+            uint hash2 = CacheKeyGenerator.Hash32(ref bytesSpan2, seed2);
+
+            // Assert
+            Assert.AreNotEqual(hash1, hash2);
+        }
+
+        [TestMethod]
+        public void SameSeeds_AndValues_HashesAreIdentical()
+        {
+            // Arrange
+            var random = new Random();
+            var bytes = new byte[64];
+            random.NextBytes(bytes);
+            var bytesSpan1 = new ReadOnlySpan<byte>(bytes);
+            var bytesSpan2 = new ReadOnlySpan<byte>(bytes);
+
+            // Act
+            uint seed = 5;
+            uint hash1 = CacheKeyGenerator.Hash32(ref bytesSpan1, seed);
+            uint hash2 = CacheKeyGenerator.Hash32(ref bytesSpan2, seed);
+
+            // Assert
+            Assert.AreEqual(hash1, hash2);
+        }
+
+        [DataTestMethod]
+        [DataRow("key", 293U, 2495785535U)]
+        [DataRow("Hello World!", 420U, 1535517821U)]
+        [DataRow("a$6ajXViSAfFw5pR2kkz3Q28YGrDx$jeaLJ5HFPe", 69U, 3131871211U)]
+        [DataRow("!zhgt#HVY#tV%kPPZ$LXYEo@EqyKjqRJPzUb3*hhASWpdyZAF3!t$V96j9Eb9ivzMH2w4jvuyHaXRxd&YbHz*W8yZGJ#CXjXfqMzNGgf@YMfh*RdZpRXtPQ3mV$N9N!%", 23485U, 4240136436U)]
+        [TestMethod]
+        public void KnownInputs_ReturnCorrectHashes(string input, uint seed, uint expected)
+        {
+            // Arrange
+            ReadOnlySpan<byte> inputSpan = Encoding.UTF8.GetBytes(input).AsSpan();
+
+            // Act
+            var actual = CacheKeyGenerator.Hash32(ref inputSpan, seed);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [DataTestMethod]
+        [DataRow(1)]
+        [DataRow(2)]
+        [DataRow(3)]
+        [DataRow(4)]
+        [DataRow(5)]
+        [DataRow(100)]
+        [DataRow(1024)]
+        [TestMethod]
+        public void DifferentInputLengths_HashesAreDistinct(int length)
+        {
+            // Arrange
+            var random = new Random();
+            var bytes = new byte[length];
+            random.NextBytes(bytes);
+            uint seed = 0;
+            var bytesSpan = new ReadOnlySpan<byte>(bytes);
+
+            // Act
+            uint hash1 = CacheKeyGenerator.Hash32(ref bytesSpan, seed);
+            bytes[0] = (byte)(bytes[0] ^ 0xFF);
+            bytesSpan = new ReadOnlySpan<byte>(bytes);
+            uint hash2 = CacheKeyGenerator.Hash32(ref bytesSpan, seed);
+
+            // Assert
+            Assert.AreNotEqual(hash1, hash2);
+        }
     }
 }
