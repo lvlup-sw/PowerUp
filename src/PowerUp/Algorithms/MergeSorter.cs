6c8e1e1cb7a65c51ef8767baf5a7f3370b94dc77
Reed Salus <reedsalus@gmail.com>
2024-09-08 00:48:04 -0700

    1.0.2: mergesort update

--- src/PowerUp/Algorithms/MergeSorter.cs
+++ src/PowerUp/Algorithms/MergeSorter.cs
@@ -12,7 +12,7 @@
         /// <param name="collection">The list to sort.</param>
         /// <param name="comparer">The comparer used to determine the order of elements. 
         /// If not provided, the default comparer for the element type is used.</param>
-        public static void MergeSort<T>(this List<T> collection, Comparer<T> comparer = default!)
+        public static void MergeSort<T>(this IList<T> collection, Comparer<T> comparer = default!)
         {
             comparer ??= Comparer<T>.Default;
             InternalMergeSort(collection, 0, collection.Count - 1, comparer);
@@ -26,7 +26,7 @@
         /// <param name="left">The index of the leftmost element in the portion to sort.</param>
         /// <param name="right">The index of the rightmost element in the portion to sort.</param>
         /// <param name="comparer">The comparer used to determine the order of elements.</param>
-        private static void InternalMergeSort<T>(List<T> collection, int left, int right, Comparer<T> comparer)
+        private static void InternalMergeSort<T>(IList<T> collection, int left, int right, Comparer<T> comparer)
         {
             if (left < right)
             {
@@ -48,16 +48,14 @@
         /// <param name="mid">The index of the rightmost element in the first sublist.</param>
         /// <param name="right">The index of the rightmost element in the second sublist.</param>
         /// <param name="comparer">The comparer used to determine the order of elements.</param>
-        private static void InternalMerge<T>(List<T> collection, int left, int mid, int right, Comparer<T> comparer)
+        private static void InternalMerge<T>(IList<T> collection, int left, int mid, int right, Comparer<T> comparer)
         {
             int leftSize = mid - left + 1;
             int rightSize = right - mid;
 
-            T[] leftArray = new T[leftSize];
-            T[] rightArray = new T[rightSize];
-
-            collection.CopyTo(left, leftArray, 0, leftSize);
-            collection.CopyTo(mid + 1, rightArray, 0, rightSize);
+            // Manually copy elements from the collection to the temporary arrays
+            T[] leftArray = collection.Skip(left).Take(leftSize).ToArray();
+            T[] rightArray = collection.Skip(mid + 1).Take(rightSize).ToArray();
 
             int i = 0, j = 0, k = left;
 
