e2eec568fc7f6c0dadb0633f3bc7b44eb3dbc75e
Reed Salus <reedsalus@gmail.com>
2024-09-17 13:43:15 -0700

    StackArrayPool final draft

--- src/PowerUp/Collections/StackedArrayPool.cs
+++ src/PowerUp/Collections/StackedArrayPool.cs
@@ -1,66 +1,86 @@
 ﻿using PowerUp.Caching;
-using PowerUp.Caching.Interfaces;
 using System.Buffers;
 using System.Collections.Concurrent;
 
 namespace PowerUp.Collections
 {
+    /// <summary>
+    /// An implementation of <see cref="ArrayPool{T}"/> utilizing a thread-local cache.
+    /// Resource eviction is handled by <see cref="FastMemCache{TKey, TValue}"/> behind the scenes.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
     public class StackArrayPool<T> : ArrayPool<T>
     {
+        // Any array less than 2^4 is cheaper to create than rent
+        // We approach max length at 2^30, thus it forms our outer bound
         private const int MinPowOf2 = 4;
         private const int MaxPowOf2 = 30;
 
-        private readonly IFastMemCache<int, ConcurrentQueue<T[]>> _buckets;
+        // This is our central pool of arrays
+        private readonly FastMemCache<int, ConcurrentQueue<T[]>> _buckets;
         private readonly int[] _bucketSizes;
 
+        // This is our local cache on each thread
+        // ThreadStatic ensures that each thread has its own independent copy of the local cache
+        // Therefore, we don't need to implement any explicit locking mechanism to handle contention
         [ThreadStatic]
-        private static T[][]? t_threadLocalCache;
+        private static T[][]? threadLocalCache;
 
+        // Time-to-live; tracked by FastMemCache
         public TimeSpan ArrayTTL { get; set; } = TimeSpan.FromMinutes(5);
 
-        public StackArrayPool(int preallocate = 0)
+        /// <summary>
+        /// Initializes a new instance of the `StackArrayPool` class, pre-allocating arrays for efficient reuse.
+        /// </summary>
+        /// <param name="preallocation">The number of arrays to pre-allocate for each bucket size (default: 1).</param>
+        public StackArrayPool(int preallocation = 1)
         {
             _bucketSizes = Enumerable.Range(MinPowOf2, MaxPowOf2 - MinPowOf2 + 1)
-                                    .Select(i => (int)Math.Pow(2, i))
-                                    .ToArray();
+                .Select(i => (int)Math.Pow(2, i))
+                .ToArray();
 
-            _buckets = new FastMemCache<int, ConcurrentQueue<T[]>>();
+            _buckets = new();
 
-            // Preallocate buckets
+            // Preallocate arrays in each bucket
             for (int i = 0; i < _bucketSizes.Length; i++)
             {
-                var queue = new ConcurrentQueue<T[]>();
-                for (int j = 0; j < preallocate; j++)
-                {
-                    queue.Enqueue(new T[_bucketSizes[i]]);
-                }
-                _buckets.AddOrUpdate(_bucketSizes[i], queue, ArrayTTL);
+                var preallocatedArrays = Enumerable.Range(0, preallocation)
+                    .Select(_ => new T[_bucketSizes[i]]);
+
+                _buckets.AddOrUpdate(
+                    _bucketSizes[i], 
+                    new ConcurrentQueue<T[]>(preallocatedArrays), 
+                    ArrayTTL
+                );
             }
         }
 
+        /// <summary>
+        /// Rents an array from the pool with a minimum specified length.
+        /// </summary>
+        /// <param name="minimumLength">The minimum required length of the rented array.</param>
+        /// <returns>An array from the pool with a length at least equal to `minimumLength`.</returns>
         public override T[] Rent(int minimumLength)
         {
+            // Check if capacity of request is within bounds
             var bucketIndex = StackArrayPool<T>.GetBucketIndex(minimumLength);
             if (bucketIndex < 0 || bucketIndex >= _bucketSizes.Length)
-            {
+            {   // Return new if out of bounds
                 return new T[minimumLength];
             }
 
             // Initialize thread-local cache if it's not already initialized
-            if (t_threadLocalCache == null)
-            {
-                t_threadLocalCache = new T[_bucketSizes.Length][];
-            }
+            threadLocalCache ??= new T[_bucketSizes.Length][];
 
             // Try to get from thread-local storage first
-            var localArray = t_threadLocalCache[bucketIndex];
-            if (localArray != null && localArray.Length >= minimumLength)
+            var localArray = threadLocalCache[bucketIndex];
+            if (localArray is not null && localArray.Length >= minimumLength)
             {
-                t_threadLocalCache[bucketIndex] = null;
+                threadLocalCache[bucketIndex] = null!;
                 return localArray;
             }
 
-            // Next, try to get an array from the cache
+            // Next, try to get an array from the central pool
             if (_buckets.TryGet(_bucketSizes[bucketIndex], out var queue) && queue.TryDequeue(out var array))
             {
                 return array;
@@ -70,40 +90,42 @@ namespace PowerUp.Collections
             return new T[_bucketSizes[bucketIndex]];
         }
 
+        /// <summary>
+        /// Returns an array to the pool for potential reuse, optionally clearing its contents.
+        /// </summary>
+        /// <param name="array">The array to be returned to the pool.</param>
+        /// <param name="clearArray">Indicates whether the array's contents should be replaced with default values before returning it (false by default).</param>
         public override void Return(T[] array, bool clearArray = false)
         {
-            if (array == null)
-                throw new ArgumentNullException(nameof(array));
+            ArgumentNullException.ThrowIfNull(array);
 
             if (clearArray)
-            {
+            {   // Replaces array contents with default values
                 Array.Clear(array, 0, array.Length);
             }
 
+            // Check if capacity of array is within bounds
             var bucketIndex = StackArrayPool<T>.GetBucketIndex(array.Length);
             if (bucketIndex < 0 || bucketIndex >= _bucketSizes.Length)
-            {
-                return; // Array is too large or too small, discard it
-            }
-
-            // Try to store in thread-local storage first
-            if (t_threadLocalCache[bucketIndex] == null)
-            {
-                t_threadLocalCache[bucketIndex] = array;
+            {   // Discard if out of bounds
                 return;
             }
 
-            var bucketSize = _bucketSizes[bucketIndex];
+            // Try to store in thread-local storage first
+            if (threadLocalCache is not null && threadLocalCache[bucketIndex] is null)
+            {
+                threadLocalCache[bucketIndex] = array;
+                return;
+            }
 
-            // Update TTL or add with TTL if it doesn't exist
-            _buckets.AddOrUpdate(bucketSize,
-                                 new ConcurrentQueue<T[]>(new[] { array }),
-                                 ArrayTTL);
+            // Otherwise, add to the central pool
+            _buckets.AddOrUpdate(
+                _bucketSizes[bucketIndex],
+                new ConcurrentQueue<T[]>([array]),
+                ArrayTTL
+            );
         }
 
-        private static int GetBucketIndex(int length)
-        {
-            return (int)Math.Log(length, 2) - MinPowOf2;
-        }
+        private static int GetBucketIndex(int length) => (int)Math.Log(length, 2) - MinPowOf2;
     }
 }
